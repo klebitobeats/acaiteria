21e8b2846d97a4691e117fc89de7f23cc7b2f31c
klebitobeats <klebsonclesio@gmail.com>
2025-07-05 15:21:43 -0300

    fix: adicionando headers CORS

--- nextjs/pages/api/create-pix-payment.js
+++ nextjs/pages/api/create-pix-payment.js
@@ -1,16 +1,19 @@
 export default async function handler(req, res) {
+  // Libera CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  // Responde requisição prévia do navegador
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
+  // SDK do Mercado Pago
   const mercadopago = require("mercadopago");
 
   mercadopago.configure({
-    access_token: "TEST-4421698743839070-050603-b2ef49b07e81b9dcd5f751d3f3c6bd01-482595074" // Substitua pelo seu token real
+    access_token: "TEST-4421698743839070-050603-b2ef49b07e81b9dcd5f751d3f3c6bd01-482595074" // Troque aqui pelo seu token real do Mercado Pago
   });
 
   try {
