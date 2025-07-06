2b56e9ca98212966b56d7ea1a5bc3610ab9095fb
klebitobeats <klebsonclesio@gmail.com>
2025-07-05 21:10:49 -0300

    fix: corrigido endpoint Mercado Pago Vercel

--- api/create-pix-payment.js
+++ api/create-pix-payment.js
@@ -1,6 +1,6 @@
-const mercadopago = require("mercadopago");
+import mercadopago from "mercadopago";
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
